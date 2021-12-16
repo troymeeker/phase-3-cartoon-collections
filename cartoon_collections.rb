c8a330b7455a5120a578811d59b3e966c286d45d
troymeeker <troymeeker1@gmail.com>
2021-12-16 09:44:38 -0800

    commit tree 84ceafa03535d7adfac6c55240a15fff19887cc9

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves names
+   names.each.with_index(1) do |n, idx|
+    puts "#{idx}. #{n}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls
+  planeteer_calls.map { |n| n.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls words_arr
+  if words_arr.any? {|n| n.length > 4}
+    true
+  else
+    false
+ end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese arr_of_strs
+cheese_types = ["cheddar", "gouda", "camembert"]
+  arr_of_strs.find do |item|
+    cheese_types.include? item
+  end
 end
